2ab86400808f8afc58554f7cc579d8890189ad4f
BOUHAFSADMI <sadmibouhafs@gmail.com>
2018-03-13 14:15:10 +0100

    added the api

--- server.js
+++ server.js
@@ -4,30 +4,28 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
-//API file for interacting with MongoDB
+// API file for interacting with MongoDB
 const api = require('./server/routes/api');
 
-
-//Parsers
+// Parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false}));
 
-
-//Angular DIST output folder
+// Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // API location
 app.use('/api', api);
 
-// send all other requests to the Angular app
+// Send all other requests to the Angular app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// set the port
+//Set Port
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
 const server = http.createServer(app);
 
-server.listen(port, () => console.log(`app running on localhost:${port}`));
+server.listen(port, () => console.log(`Running on localhost:${port}`));
---
+++ server/routes/api.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
+const assert = require('assert');
+
+// Error handling
+const sendError = (err, res) => {
+    response.status = 501;
+    response.message = typeof err == 'object' ? err.message : err;
+    res.status(501).json(response);
+};
+
+// Response handling
+let response = {
+    status: 200,
+    data: [],
+    message: null
+};
+
+// Get users
+router.get('/users', (req, res) => {
+
+  MongoClient.connect('mongodb://localhost:27017', function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db('mean');
+
+    db.collection('users')
+        .find()
+        .toArray()
+        .then((users) => {
+            response.data = users;
+            res.json(response);
+        })
+        .catch((err) => {
+            sendError(err, res);
+        });
+    client.close();
+  });
+});
+
+module.exports = router;
